aaac3f23dcdb61684f5161473b582ed619a1d022
dlu19 <naivar@pm.me>
2024-06-13 21:14:42 -0600

    This program is now functional. More will be added in the future.main

--- main.py
+++ main.py
@@ -5,11 +5,8 @@ from variables import Variables
 import subprocess as sp
 import os
 import sys
-import ffmpeg
-import ctypes
 
 def main():
-
     sp.run("python testing.py", shell=True)
     sp.run("cls" if os.name == "nt" else "clear", shell=True)
     print('''
@@ -22,31 +19,29 @@ def main():
 
           ''')
     # Check if ffmpeg is installed
-    if not sp.run(["ffmpeg", "-version"], stdout=sp.DEVNULL, stderr=sp.STDOUT).returncode == 0:
+    if sp.run(["ffmpeg", "-version"], stdout=sp.DEVNULL, stderr=sp.STDOUT).returncode != 0:
         print("ffmpeg is not installed or not accessible.")
         raise SystemExit()
 
-    start = input("Press Enter to start the process, or press 'q' to quit.")
+    start = input("Press Enter to start the process, or press 'q' to quit: ")
     if start == "":
         sp.run("cls" if os.name == "nt" else "clear", shell=True)
         variables = Variables()
         variables.name_video()
         variables.interface()
-    elif start == "q":
+    elif start.lower() == "q":
         print("Goodbye!")
         sys.exit()
     else:
         print("Invalid input. Please press Enter to start the process, or press 'q' to quit.")
         raise SystemExit()
 
-
-
     print()
     query = input("Would you like to generate binaural beats? (y/n): ")
     result = None
-    if query == "y":
+    if query.lower() == "y":
         binaural_beat_generator = BinauralBeatGenerator(variables)
-        result = binaural_beat_generator.binaural_beats(variables)
+        result = binaural_beat_generator.binaural_beats()
     if result is None:
         print("Error generating binaural beats.")
         raise SystemExit()
@@ -58,16 +53,15 @@ def main():
     # Create an instance of BinauralBeatMixer and call the methods
     query = input("Would you like to mix the binaural beats with the video file? (y/n): ")
     result = None
-    if query == "y":
+    if query.lower() == "y":
         binaural_beat_mixer = BinauralBeatMixer(variables)
-        result = binaural_beat_mixer.binauralmixer()
+        result = binaural_beat_mixer.binaural_mixer()
     if result is None:
         print("Error mixing binaural beats.")
         raise SystemExit()
     else:
         print("Binaural beats successfully mixed.")
-        print("Ready to mix the binaural beats with the video file.")
-        print("The output file is named output.wav.")
+        print("The output file is named output.mp4.")
         print("Thank you for using the Binaural Beat Generator and Mixer.")
         print("Goodbye!")
         raise SystemExit()
